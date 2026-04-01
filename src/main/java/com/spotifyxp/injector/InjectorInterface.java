a2053613eacc9a3e4ac160f2f66374f5efc4fe3f
werwolf2303 <gianlucabeil2303@outlook.de>
2026-04-01 07:15:40 +0200

    Remove deprecated methods in InjectorInterface

--- src/main/java/com/spotifyxp/injector/InjectorInterface.java
+++ src/main/java/com/spotifyxp/injector/InjectorInterface.java
@@ -15,34 +15,6 @@
  */
 package com.spotifyxp.injector;
 
-import java.util.ArrayList;
-
 public interface InjectorInterface {
-    /**
-     * Will be removed in favor of the plugin.json
-     */
-    @Deprecated
-    default String getIdentifier() {return "";}
-
-    /**
-     * Will be removed in favor of the plugin.json
-     */
-    @Deprecated
-    default String getVersion() {return "";}
-
-    /**
-     * Will be removed in favor of the plugin.json
-     */
-    @Deprecated
-    default String getAuthor() {return "";}
-
-    /**
-     * Will be removed in favor of the plugin.json
-     */
-    @Deprecated
-    default ArrayList<Injector.Dependency> getDependencies() {
-        return new ArrayList<>();
-    }
-
     void init();
 }
